1c594cb454ad926e14f969ab294d384356b2707a
faharAbdullahKhan <faharkhaan@gmail.com>
2024-02-03 07:07:29 +0500

    minor changes

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -4,11 +4,12 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:login_page/Dashboard/MainScreen/dashboard.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:http_parser/http_parser.dart';
 import 'package:login_page/services/api_method.dart';
 import 'package:login_page/utils/app_constants.dart';
 import 'package:login_page/utils/logs.dart';
-import 'package:http/http.dart' as http;
+
 
 class ApiClient {
   //
@@ -71,42 +72,39 @@ class ApiClient {
   //   }
   // }
 
-  static const baseUrl = "http://127.0.0.1:8000/api/";
-
-  static Future<void> login({
-    required BuildContext context,
+  static Future<dio.Response> login({
     required String email,
-    required String password,
+    required String password, required BuildContext context,
   }) async {
+    var url = AppConstants.BASE_URL + AppConstants.LOGIN_URL;
+    dio.Response response;
+    response = await ApiMethods.postRequest(
+        {
+          "Authorization": "Bearer 1|68CBxqGnar60LnDD8kOwTDxOyROZawPDouHQkZy0",
+          "Content-Type": "application/json"},
+        data: {"email": email, "password": password}, url: url);
+
     try {
-      final url = await Uri.parse("http://localhost:8000/api/loginRest");
-      // final url = await Uri.parse("http://127.0.0.1:8000/api/leave");
-      print("-------------------------------------parsed--------------------");
-      var response = await http.post(url, headers: {
-        'Authorization': 'Bearer 1|68CBxqGnar60LnDD8kOwTDxOyROZawPDouHQkZy0',
-        'Accept': 'application/json',
-      }, body: {
-        "email": email,
-        "password": password
-      });
-      print(
-          "-----------------------------------------response.body-----------------");
-      print("response.statusCode ${response.statusCode}");
-      if (response.statusCode == 200) {
-        print("response is 200");
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => DashboardScreen(),
-          ),
-        );
+      logs(response.statusMessage.toString());
+      if (response.statusCode! == 200) {
+        return response;
       } else {
-        throw Exception('Failed to load leaves');
+        return response;
       }
-    } catch (error) {
-      print(error);
-      throw Exception('Error fetching leaves: $error');
+    } on dio.DioError catch (e) {
+      if (e.message == "Receiving data timeout[50000ms]") {
+        return response;
+      }
+      if (e.message == "Http status error [401]") {
+        return response;
+      } else if (e.message == "Http status error [500]") {
+        return response;
+      }
+      return response;
     }
   }
+
+
 
   //
   // static Future<dio.Response> verifyOtp({
@@ -142,40 +140,41 @@ class ApiClient {
   //   }
   // }
   //
-  // static Future<dio.Response> getTasks() async {
-  //   var url = AppConstants.BASE_URL + AppConstants.GET_TASKS;
-  //   dio.Response response;
-  //   response = await ApiMethods.getMethod(headers: {
-  //     // "Authorization": "Bearer ${Preferences.getAuthId()}",
-  //     "Content-Type": "application/json",
-  //   }, url: url);
-  //   try {
-  //     String responseJson = json.encode(response.data);
-  //     logs(response.statusCode.toString());
-  //     if (response.statusCode! == 200) {
-  //       return response;
-  //     }else {
-  //       return response;
-  //
-  //     }
-  //   } on dio.DioError catch (e) {
-  //     if (e.message == "Receiving data timeout[50000ms]") {
-  //       // return ("Please check your connection");
-  //       return response;
-  //
-  //     }
-  //     if (e.message == "Http status error [401]") {
-  //       // return ("401");
-  //       return response;
-  //
-  //     } else if (e.message == "Http status error [500]") {
-  //       return response;
-  //
-  //       // return ("500");
-  //     }
-  //     // return (e.message!);
-  //     return response;
-  //
-  //   }
-  // }
+  static Future<dio.Response> getTasks() async {
+    var url = AppConstants.BASE_URL + AppConstants.GET_TASKS;
+    dio.Response response;
+    response = await ApiMethods.getMethod(headers: {
+      // "Authorization": "Bearer ${Preferences.getAuthId()}",
+      "Content-Type": "application/json",
+    }, url: url);
+    try {
+      String responseJson = json.encode(response.data);
+      logs(response.statusCode.toString());
+      if (response.statusCode! == 200) {
+        return response;
+      }else {
+        return response;
+
+      }
+    } on dio.DioError catch (e) {
+      if (e.message == "Receiving data timeout[50000ms]") {
+        // return ("Please check your connection");
+        return response;
+
+      }
+      if (e.message == "Http status error [401]") {
+        // return ("401");
+        return response;
+
+      } else if (e.message == "Http status error [500]") {
+        return response;
+
+        // return ("500");
+      }
+      // return (e.message!);
+      return response;
+
+    }
+  }
+
 }
